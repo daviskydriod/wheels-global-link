cb45ba70818c38bfac2441ab37154b11bc2e8e95
daviskydriod <67622791+daviskydriod@users.noreply.github.com>
2026-09-18 07:04:04 +0100

    Delete src directory

--- src/routes/cars/brands.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/cars/brands")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/cars/brands"!</div>;
-}
